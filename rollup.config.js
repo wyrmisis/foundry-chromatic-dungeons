9a9a680b85ffefe751c086d0d2f1eda09c08f2ed
wyrmisis <tim.snyder.work@gmail.com>
2021-10-08 08:47:17 -0400

    Only run Livereload in dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,8 @@
-// Node modules
-import glob from "glob";
-import path from "path";
-
 // Rollup plugins
 import { nodeResolve }  from '@rollup/plugin-node-resolve';
 import { terser }       from "rollup-plugin-terser";
 import copy             from "rollup-plugin-copy-assets";
 import sourcemaps       from "rollup-plugin-sourcemaps";
-import livereload       from 'rollup-plugin-livereload';
-
-const watcher = (globs) => ({
-  buildStart() {
-    for (const item of globs) {
-      glob.sync(path.resolve(item)).forEach((filename) => {
-        this.addWatchFile(filename);
-      });
-    }
-  }
-})
 
 export default {
   input: './src/module/index.mjs',
@@ -34,8 +19,6 @@ export default {
     }
   ],
   plugins: [
-    watcher(['src/**/*.hbs', 'src/**/*.html']),
-    livereload('dist'),
     sourcemaps(),
     nodeResolve({ browser: true }),
     copy({
