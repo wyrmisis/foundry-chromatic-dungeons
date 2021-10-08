b9d5e20fc401f7ad5ff8efc67e6aee88c21091ca
wyrmisis <tim.snyder.work@gmail.com>
2021-10-07 20:04:35 -0400

    Quick sidebar to get livereload and sourcemaps in.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,42 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
-import copy from "rollup-plugin-copy-assets";
+// Node modules
+import glob from "glob";
+import path from "path";
+
+// Rollup plugins
+import { nodeResolve }  from '@rollup/plugin-node-resolve';
+import { terser }       from "rollup-plugin-terser";
+import copy             from "rollup-plugin-copy-assets";
+import sourcemaps       from "rollup-plugin-sourcemaps";
+import livereload       from 'rollup-plugin-livereload';
+
+const watcher = (globs) => ({
+  buildStart() {
+    for (const item of globs) {
+      glob.sync(path.resolve(item)).forEach((filename) => {
+        this.addWatchFile(filename);
+      });
+    }
+  }
+})
 
 export default {
   input: './src/module/index.mjs',
   output: [
-    { file: './dist/js/bundle.js' },
-    { file: './dist/js/bundle.min.js', plugins: [terser()], format: 'iife' }
+    {
+      file: './dist/js/bundle.js',
+      sourcemaps: true
+    },
+    {
+      file: './dist/js/bundle.min.js',
+      plugins: [terser()],
+      sourcemap: true,
+      format: 'iife'
+    }
   ],
   plugins: [
+    watcher(['src/**/*.hbs', 'src/**/*.html']),
+    livereload('dist'),
+    sourcemaps(),
     nodeResolve({ browser: true }),
     copy({
       assets: [
--- src/module/index.mjs
+++ src/module/index.mjs
@@ -1,3 +1,13 @@
+Hooks.once('devModeReady', () => {
+  // Only add this script for livereload if we're using Developer Mode
+  // (https://github.com/League-of-Foundry-Developers/foundryvtt-devMode)
+  const src = `http://${(location.host || 'localhost').split(':')[0]}:35729/livereload.js?snipver=1`
+  const script = document.createElement('script');
+  script.src = src;
+
+  document.body.appendChild(script);
+})
+
 /* -------------------------------------------- */
 /*  Init Hook                                   */
 /* -------------------------------------------- */
